4226586b58dee72215037bd59eb63aa0f64127ee
张经纬 <dev16101@linx-info.com>
2022-11-18 14:08:20 +0800

    Add structured logger for cpds-detector

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,21 @@
 package main
 
-func main() {
+import (
+	"os"
 
-	return
+	"github.com/sirupsen/logrus"
+)
+
+func initLogging() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	logrus.SetOutput(os.Stdout)
+}
+
+func main() {
+	initLogging()
+
+	logrus.Infof("Starting cpds-detector...")
 }
