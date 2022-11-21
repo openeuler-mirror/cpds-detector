f4f3c74b0b79c112e66f1ba16a58e6c81c37a716
张经纬 <dev16101@linx-info.com>
2022-11-21 17:20:49 +0800

    Add debug configuration

--- config/options.go
+++ config/options.go
@@ -1,8 +1,12 @@
 package config
 
-import "github.com/spf13/pflag"
+import (
+	"github.com/spf13/pflag"
+)
 
 type Options struct {
+	Debug           bool
+	LogLevel        string
 	DatabaseAddress string
 	DatabasePort    string
 }
@@ -12,6 +16,9 @@ func NewOptions() *Options {
 }
 
 func (o *Options) InstallFlags(flags *pflag.FlagSet) {
+	flags.BoolVarP(&o.Debug, "debug", "D", false, "Enable debug mode")
+	flags.StringVarP(&o.LogLevel, "log-level", "l", "info", `Set the logging level ("debug"|"info"|"warn"|"error"|"fatal")`)
+
 	flags.StringVar(&o.DatabaseAddress, "db_address", "localhost", "specify database address")
 	flags.StringVar(&o.DatabasePort, "db_port", "3306", "specify database port")
 }
--- detector/detector.go
+++ detector/detector.go
@@ -1,13 +1,60 @@
 package detector
 
 import (
+	"fmt"
+	"os"
+
 	"gitee.com/cpds/cpds-detector/config"
 	"github.com/sirupsen/logrus"
 )
 
 func RunDetector(opts *config.Options) error {
+	if err := configureLogLevel(opts); err != nil {
+		return err
+	}
+
+	if opts.Debug {
+		enableDebug()
+		logrus.Debugf("Enable debug mode")
+	}
+
 	logrus.Infof("Starting cpds-detector......")
 	logrus.Infof("Using config: database address: %s, database port: %s", opts.DatabaseAddress, opts.DatabasePort)
 	// TODO: complete this function
+	return nil
+}
+
+// enableDebug sets the DEBUG env var to true
+// and makes the logger to log at debug level.
+func enableDebug() {
+	os.Setenv("DEBUG", "1")
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+// disableDebug sets the DEBUG env var to false
+// and makes the logger to log at info level.
+func disableDebug() {
+	os.Setenv("DEBUG", "")
+	logrus.SetLevel(logrus.InfoLevel)
+}
+
+// isDebugEnabled checks whether the debug flag is set or not.
+func isDebugEnabled() bool {
+	return os.Getenv("DEBUG") != ""
+}
+
+// configureLogLevel "debug"|"info"|"warn"|"error"|"fatal", default: "info"
+func configureLogLevel(opts *config.Options) error {
+	if opts.LogLevel != "" {
+		lvl, err := logrus.ParseLevel(opts.LogLevel)
+		if err != nil {
+			return fmt.Errorf("unable to parse logging level: %s", opts.LogLevel)
+		}
+		logrus.SetLevel(lvl)
+	} else {
+		// Set InfoLevel as default logLevel
+		// Only log the info severity or above.
+		logrus.SetLevel(logrus.InfoLevel)
+	}
 	return nil
 }
