cde2feea04c71fba88e36688c0b7302beb9ed0e1
张经纬 <dev16101@linx-info.com>
2022-11-29 19:58:40 +0800

    Complete API /rules

--- pkgs/apis/rules/v1/register.go
+++ pkgs/apis/rules/v1/register.go
@@ -9,9 +9,9 @@ func AddToContainer(container *restful.Container, r *rules.Rules) {
 	webservice := new(restful.WebService)
 	webservice.Path("/rules")
 
-	handler := newRulesHandler()
+	handler := newRulesHandler(r)
 
-	webservice.Route(webservice.GET("/").
+	webservice.Route(webservice.GET("").
 		To(handler.GetRules))
 
 	container.Add(webservice)
