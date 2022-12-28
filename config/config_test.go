86603b648bd96e2e9c60eaf64676d0f32084c4bc
张经纬 <dev16101@linx-info.com>
2022-12-28 20:10:43 +0800

    Add test case: test load config function

--- config/config_test.go
+++ config/config_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spf13/pflag"
 	"gotest.tools/assert"
+	is "gotest.tools/assert/cmp"
 )
 
 func TestCheckConfig(t *testing.T) {
@@ -24,4 +25,17 @@ func TestCheckConfig(t *testing.T) {
 		"--db-address=1234",
 	})
 	assert.Error(t, conf.CheckConfig(), "invalid flag: db-address: 1234")
+}
+
+func TestLoadConfig(t *testing.T) {
+	conf := New()
+	flags := pflag.NewFlagSet("testing", pflag.ContinueOnError)
+
+	flags.Parse([]string{
+		"--config-path=config/json/cpds-detector.json",
+	})
+	conf.LoadConfig(flags)
+
+	assert.Check(t, is.Equal(conf.DatabaseUser, "root"))
+	assert.Check(t, is.Equal(conf.Port, "19081"))
 }
