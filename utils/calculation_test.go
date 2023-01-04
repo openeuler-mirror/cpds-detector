b435b3dfbedf6e0329fd831b53fede2b31f9c851
张经纬 <dev16101@linx-info.com>
2023-01-04 09:37:30 +0800

    Add test case: test get mean function

--- utils/calculation_test.go
+++ utils/calculation_test.go
@@ -16,3 +16,18 @@ func TestGetSum(t *testing.T) {
 	}
 	assert.Equal(t, sum, GetSum(arr...))
 }
+
+func TestGetMean(t *testing.T) {
+	arr := make([]float64, 0)
+	arr = append(arr, 1.1, 2.2, 3.3, 4.4, 5.5)
+
+	var m1, sum float64
+	for _, v := range arr {
+		sum += v
+	}
+	m1 = sum / float64(len(arr))
+	m2, err := GetMean(arr...)
+
+	assert.Check(t, err)
+	assert.Equal(t, m1, m2)
+}
