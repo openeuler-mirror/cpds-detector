8e337401699b9e7d9bb0ed1a46b48ebe73c2d93f
张经纬 <dev16101@linx-info.com>
2023-01-09 19:43:31 +0800

    Add test case: test get min function

--- utils/calculation_test.go
+++ utils/calculation_test.go
@@ -77,3 +77,14 @@ func TestGetMaxValue(t *testing.T) {
 	assert.Check(t, err)
 	assert.Equal(t, 5.5, max)
 }
+
+func TestGetMinValue(t *testing.T) {
+	arr := make([]float64, 0)
+	min, err := GetMaxValue(arr...)
+	assert.Error(t, err, "invalid argument: array cannot be empty")
+
+	arr = append(arr, 1.1, 2.2, 5.5, 4.4, 3.3)
+	min, err = GetMaxValue(arr...)
+	assert.Check(t, err)
+	assert.Equal(t, 1.1, min)
+}
