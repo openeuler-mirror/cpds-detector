6658edfd6d21a0442b1d2f872a4548295a273538
张经纬 <dev16101@linx-info.com>
2023-02-01 15:58:51 +0800

    add db-password flag test case

--- config/options_test.go
+++ config/options_test.go
@@ -21,6 +21,7 @@ func TestInstallFlags(t *testing.T) {
 		"--log-level=error",
 		"--db-port=7890",
 		"--db-user=test",
+		"--db-password=password1",
 	})
 
 	assert.Check(t, err)
@@ -31,4 +32,5 @@ func TestInstallFlags(t *testing.T) {
 	assert.Equal(t, conf.LogLevel, "error")
 	assert.Equal(t, conf.DatabasePort, "7890")
 	assert.Equal(t, conf.DatabaseUser, "test")
+	assert.Equal(t, conf.DatabasePassword, "password1")
 }
