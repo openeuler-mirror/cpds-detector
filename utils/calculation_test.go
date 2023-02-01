69494ffd0a32e6eddc2094412a9148230038533e
张经纬 <dev16101@linx-info.com>
2023-02-01 10:35:29 +0800

    update get max value test case

--- utils/calculation_test.go
+++ utils/calculation_test.go
@@ -88,6 +88,8 @@ func TestGetStandardDeviation(t *testing.T) {
 func TestGetMaxValue(t *testing.T) {
 	arr := make([]float64, 0)
 	max, err := GetMaxValue(arr...)
+
+	assert.Equal(t, max, float64(-1))
 	assert.Error(t, err, "invalid argument: array cannot be empty")
 
 	arr = append(arr, 1.1, 2.2, 5.5, 4.4, 3.3)
