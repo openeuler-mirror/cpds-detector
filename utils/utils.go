61159946deec9f82757aebeeb909ef45a7c1ed70
张经纬 <dev16101@linx-info.com>
2023-02-02 09:39:22 +0800

    add util: print whether there is a number in the array

--- utils/utils.go
+++ utils/utils.go
@@ -46,3 +46,12 @@ func GetDifferentIntArray(sourceList, sourceList2 []int) (result []int) {
 	}
 	return
 }
+
+func ExistIntArray(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
