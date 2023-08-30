4d52564e96b5c4b708997b25d9ad2bd6f65d4ec9
SaarHV <dev16102@linx-info.com>
2023-08-30 17:56:49 +0800

    Initialize Database

--- internal/core/analysis.go
+++ internal/core/analysis.go
@@ -1,12 +1,12 @@
-/* 
+/*
  *  Copyright 2023 CPDS Author
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *       https://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -180,6 +180,7 @@ func insertRecord(id uint, name, kind string, metric *prometheus.Metric) error {
 	}
 
 	db := r.db.Session(&gorm.Session{}).Model(&Analysis{})
+	db = db.Exec("USE cpds")
 	result := db.Where("rule_id = ?", id).Order("update_time DESC").Limit(1).Find(&existingRecord)
 	if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		return result.Error
